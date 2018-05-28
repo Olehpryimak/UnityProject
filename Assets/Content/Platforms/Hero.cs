ac68b2507ad0a49334d9049d69d3f43da1c2d3fc
Olehpryimak <31596938+Olehpryimak@users.noreply.github.com>
2018-05-28 14:35:54 +0300

    Paralax and animations commit

--- Assets/Content/Platforms/Hero.cs
+++ Assets/Content/Platforms/Hero.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Hero : MonoBehaviour {
 
     public float speed = 1;
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
     Rigidbody2D myBody = null;
     // Use this for initialization
     void Start () {
@@ -21,24 +26,80 @@ public class Hero : MonoBehaviour {
     {
     
         float value = Input.GetAxis("Horizontal");
-        float valueY = Input.GetAxis("Vertical");
-        Animator animator = GetComponent<Animator>();
-        
-        if ((valueY) > 0)
-        {
-            Vector2 vel = myBody.velocity;
-            vel.y = valueY * speed;
 
-            myBody.velocity = vel;
-            animator.SetBool("jump", true);
-           
+
+
+
+
+        
+
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
         }
         else
         {
-            
+            isGrounded = false;
+        }
+
+        Debug.DrawLine(from, to, Color.red);
+
+
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        Animator animator = GetComponent<Animator>();
+
+
+
+
+
+        if (this.isGrounded)
+        {
             animator.SetBool("jump", false);
         }
-            if (Mathf.Abs(value) > 0)
+        else
+        {
+            animator.SetBool("jump", true);
+        }
+        if (Mathf.Abs(value) > 0)
         {
             Vector2 vel = myBody.velocity;
             vel.x = value * speed;
